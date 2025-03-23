d25f77afb044a29df2976cdc7a695ca67bbb8097
Bhagirath Bhatti <bhattibhagirath4618@gmail.com>
2025-03-24 00:26:42 +0530

    configue

--- next.config.js
+++ next.config.js
@@ -6,9 +6,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true, // Enable if using App Router
-  },
 };
 
 module.exports = nextConfig;
