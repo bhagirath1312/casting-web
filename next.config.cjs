f12650ef4bf0676a368320fd1d6546f91beedb8f
Bhagirath Bhatti <bhattibhagirath4618@gmail.com>
2025-03-24 00:23:18 +0530

    configuer next

---
+++ next.config.cjs
@@ -0,0 +1,11 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+// next.config.cjs (CommonJS)
+module.exports = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Enable if using App Router
+  },
+};
